40b4a2ec83b428876beea58f63826cdccae858f2
giuleon <deluca.giuliano@gmail.com>
2017-10-14 23:36:21 +0200

    added automated deploy of an SPFx in the app catalog and minor improvements

--- DeploySPFxToAppCatalog.ps1
+++ DeploySPFxToAppCatalog.ps1
@@ -1,9 +1,9 @@
 #################
 # Configuration #
 #################
-$catalogSite = "https://giuleon.sharepoint.com/sites/apps"
-$catalogName = "AppCatalog"
-$catalogRelativePath = "sites/apps/AppCatalog"
+$catalogSite = "https://giuleon.sharepoint.com/sites/apps" # => Insert your catalog site
+$catalogName = "AppCatalog" # => the catalog name
+$catalogRelativePath = "sites/apps/AppCatalog" # => app catalog site relative url
 #######
 # End #
 #######
@@ -12,37 +12,34 @@ function GetRequest ($apiUrl, $webSession) {
     return Invoke-WebRequest -Uri $apiUrl -Method Get -WebSession $webSession
 }
 
-function replaceXml($lines, $old, $new) {
-    $len = $lines.count
-    for($i=0;$i-lt$len;$i++){
-        $lines[$i] = $lines[$i] -replace $old, $new
-    }
-    return $lines
+function PostRequest($apiUrl, $webSession, $body) {
+    return Invoke-WebRequest -Uri $apiUrl -Body $body -Method Post -WebSession $webSession
+   
 }
 
 function setXmlMapping($xmlBody, $siteId, $webId, $listId, $fileId, $fileVersion, $skipDeployment) {
     # Replace the random token with a random guid
     $randomGuid = [guid]::NewGuid()
-    #$xmlBody = $xmlBody.replace(new [RegExp]::('\\{randomId\\}', 'g'), $randomGuid)
-    $xmlBody = replaceXml -lines $xmlBody -old '{randomId}' -new $randomGuid
+    $skipDep = $skipDeployment
+    if($skipDeployment -eq $True){
+        $skipDep = "true"
+    }
+    else{
+        $skipDep = "false"
+    }
+    $xmlBody = [regex]::replace($xmlBody, "{randomId}", $randomGuid) #$xmlBody.replace([RegExp]::("\\{randomId\\}", "g"), $randomGuid)
     # Replace the site ID token with the actual site ID string
-    #$xmlBody = xmlBody.replace(new RegExp('\\{siteId\\}', 'g'), siteId)
-    $xmlBody = replaceXml -lines $xmlBody -old '{siteId}' -new $siteId
+    $xmlBody = [regex]::replace($xmlBody, "{siteId}", $siteId)
     # Replace the web ID token with the actual web ID string
-    #$xmlBody = xmlBody.replace(new RegExp('\\{webId\\}', 'g'), webId)
-    $xmlBody = replaceXml -lines $xmlBody -old '{webId}' -new $webId
+    $xmlBody = [regex]::replace($xmlBody, "{webId}", $webId)
     # Replace the list ID token with the actual list ID string
-    #$xmlBody = xmlBody.replace(new RegExp('\\{listId\\}', 'g'), listId)
-    $xmlBody = replaceXml -lines $xmlBody -old '{listId}' -new $listId
+    $xmlBody = [regex]::replace($xmlBody, "{listId}", $listId)
     # Replace the item ID token with the actual item ID number
-    #$xmlBody = xmlBody.replace(new RegExp('\\{itemId\\}', 'g'), $fileId)
-    $xmlBody = replaceXml -lines $xmlBody -old '{itemId}' -new $fileId
+    $xmlBody = [regex]::replace($xmlBody, "{itemId}", $fileId)
     # Replace the file version token with the actual file version number
-    #$xmlBody = xmlBody.replace(new RegExp('\\{fileVersion\\}', 'g'), $fileVersion)
-    $xmlBody = replaceXml -lines $xmlBody -old '{fileVersion}' -new $fileVersion
+    $xmlBody = [regex]::replace($xmlBody, "{fileVersion}", $fileVersion)
     # Replace the skipFeatureDeployment token with the skipFeatureDeployment option
-    #$xmlBody = xmlBody.replace(new RegExp('\\{skipFeatureDeployment\\}', 'g'), $skipDeployment)
-    $xmlBody = replaceXml -lines $xmlBody -old '{skipFeatureDeployment}' -new $skipDeployment
+    $xmlBody = [regex]::replace($xmlBody, "{skipFeatureDeployment}", $skipDep)
     return $xmlBody;
 }
 
@@ -52,20 +49,19 @@ Write-Host ***************************************** -ForegroundColor Yellow
 $packageConfig = Get-Content -Raw -Path .\config\package-solution.json | ConvertFrom-Json
 $packagePath = Join-Path "sharepoint/" $packageConfig.paths.zippedPackage -Resolve
 $skipFeatureDeployment = $packageConfig.solution.skipFeatureDeployment
-<#
-Connect-PnPOnline –Url $catalogSite –Credentials $cred
+
+Connect-PnPOnline $catalogSite -Credentials giuleon
 Add-PnPFile -Path $packagePath -Folder $catalogName
 
 Write-Host *************************************************** -ForegroundColor Yellow
 Write-Host * The SPFx solution has been succesfully deployed * -ForegroundColor Yellow
 Write-Host *************************************************** -ForegroundColor Yellow
-#>
 
 # Connect to SharePoint Online
 $targetSite = "https://giuleon.sharepoint.com/sites/apps"
 $targetSiteUri = [System.Uri]$targetSite
  
-Connect-PnPOnline $targetSite -Credentials giuleon
+#Connect-PnPOnline $targetSite -Credentials giuleon
  
 # Retrieve the client credentials and the related Authentication Cookies
 $context = (Get-PnPWeb).Context
@@ -76,40 +72,46 @@ $authenticationCookies = $credentials.GetAuthenticationCookie($targetSiteUri, $t
 $webSession = New-Object Microsoft.PowerShell.Commands.WebRequestSession  
 $webSession.Cookies.SetCookies($targetSiteUri, $authenticationCookies)
 $webSession.Headers.Add("Accept", "application/json;odata=verbose")
+
+$apiUrl = $catalogSite + "/_api/contextinfo?$"+"select=FormDigestValue"
+$result = PostRequest -apiUrl $apiUrl -webSession $webSession
+$formDigest = $result.Content | ConvertFrom-Json
+Write-Host "FormDigestValue - " $formDigest.d.GetContextWebInformation.FormDigestValue
+$formDigest = $formDigest.d.GetContextWebInformation.FormDigestValue
+$webSession.Headers.Add("X-RequestDigest", $formDigest)
  
 # Set request variables
-#$apiUrl = "$targetSite" + "_api/web/currentuser"
 $apiUrl = "$targetSite" + "/_api/site?$"+"select=Id"
 
 # Make the REST request
 $webRequest =  GetRequest -apiUrl $apiUrl -webSession $webSession # Invoke-WebRequest -Uri $apiUrl -Method Get -WebSession $webSession
 $response = $webRequest.Content | ConvertFrom-Json
 $siteId = $response.d.Id
-Write-Host $response.d.Id
+Write-Host "Site Id - " $response.d.Id
 
 # Retrieving webId and listId
-$apiUrl = "$targetSite" + "/_api/web/getList('$catalogRelativePath')?$"+"select=Id,ParentWeb/Id&$"+"expand=ParentWeb"
+$apiUrl = "$targetSite" + "/_api/web/getList('$catalogRelativePath')?$"+"select=Id,ParentWeb/Id`&`$"+"expand=ParentWeb"
 $webRequest =  GetRequest -apiUrl $apiUrl -webSession $webSession # Invoke-WebRequest -Uri $apiUrl -Method Get -WebSession $webSession
 $response = $webRequest.Content | ConvertFrom-Json
 $webId = $response.d.ParentWeb.Id
 $listId = $response.d.Id
-Write-Host $response.d.Id
+Write-Host "Web Id - " $webId " / List Id - " + $listId
 
 # Get the ListItemAllFields Id and Version
 $fileName = $packageConfig.paths.zippedPackage.Substring($packageConfig.paths.zippedPackage.LastIndexOf("/")+1)
-$apiUrl = "$targetSite" + "/_api/web/GetFolderByServerRelativeUrl('AppCatalog')/Files('$fileName')?$"+"expand=ListItemAllFields&$" + "select=ListItemAllFields/ID,ListItemAllFields/owshiddenversion"
-$webRequest =  GetRequest -apiUrl $apiUrl -webSession $webSession # Invoke-WebRequest -Uri $apiUrl -Method Get -WebSession $webSession
+$apiUrl = "$targetSite" + "/_api/web/GetFolderByServerRelativeUrl('AppCatalog')/Files('$fileName')?$"+"expand=ListItemAllFields`&`$" + "select=ListItemAllFields/ID,ListItemAllFields/owshiddenversion"
+$webRequest =  GetRequest -apiUrl $apiUrl -webSession $webSession 
 $response = $webRequest.Content -replace '"id":', '"id_":' | ConvertFrom-Json
 $fileId = $response.d.ListItemAllFields.id_
-$fileVersion = $response.ListItemAllFields.owshiddenversion
-Write-Host $response.d.ListItemAllFields.id_
+$fileVersion = $response.d.ListItemAllFields.owshiddenversion
+Write-Host "ListItem Id - " $fileId  " / Version - " $fileVersion
 
 # Read the xml
-$xmlBody = Get-Content DeplosSPFxToAppCatalogRequestBody.xml
+$xmlBody = Get-Content DeploySPFxToAppCatalogRequestBody.xml -Encoding UTF8
 $xmlBody = setXmlMapping -xmlBody $xmlBody -siteId $siteId -webId $webId -listId $listId -fileId $fileId -fileVersion $fileVersion -skipDeployment $skipFeatureDeployment
 Write-Host $xmlBody
 
-# Consume the JSON result
-#$jsonLibrary = $webRequest.Content | ConvertFrom-Json
-#Write-Host $jsonLibrary.d.Title
-
+$webSession.Headers.Add("Content-type", "application/xml")
+$apiUrl = $catalogSite + "/_vti_bin/client.svc/ProcessQuery"
+$result = PostRequest -apiUrl $apiUrl -webSession $webSession -body $xmlBody
+Write-Host $result
--- DeploySPFxToO365CDN.ps1
+++ DeploySPFxToO365CDN.ps1
@@ -1,13 +1,11 @@
 ﻿#################
 # Configuration #
 #################
-$cdnSite = "https://giuleon.sharepoint.com/"
-$cdnLib = "cdn/SPFx-deploy"
+$cdnSite = "https://giuleon.sharepoint.com/" # => CDN SharePoint site
+$cdnLib = "cdn/SPFx-deploy" # => Document library and eventual folders
 #######
 # End #
 #######
-
-$cred = Get-Credential
 
 Write-Host ************************************************************************************** -ForegroundColor Yellow
 Write-Host * Reading the cdnBasePath from write-manifests.json and collectiong the bundle files * -ForegroundColor Yellow
@@ -19,7 +17,7 @@ $files = Get-ChildItem $bundlePath\*.*
 Write-Host **************************************** -ForegroundColor Yellow
 Write-Host Uploading the bundle on Office 365 CDN * -ForegroundColor Yellow
 Write-Host **************************************** -ForegroundColor Yellow
-Connect-PnPOnline –Url $cdnSite –Credentials $cred
+Connect-PnPOnline $cdnSite -Credentials giuleon
 foreach ($file in $files) {
     $fullPath = $file.DirectoryName + "\" + $file.Name
     Add-PnPFile -Path $fullPath -Folder $cdnLib
